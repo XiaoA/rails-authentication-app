62ace91c3f21c60d1733ba8b880c6a11fe17fb67
Andrew Buckingham <abuckingham99@gmail.com>
2022-02-13 14:55:41 -0800

    Fix URL in session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "aeb-rails-authentication-app.heroku.com"
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://aeb-rails-authentication-app.herokuapp.com"
 else
   Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
