9a607c371462d91d7d6f0e7a16582b9167f310bd
Andrew Buckingham <abuckingham99@gmail.com>
2022-02-13 14:41:46 -0800

    Fix typo in Registrations Controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < ApplicationController
     user = User.create!(
       email: params['user']['email'],
       password: params['user']['password'],
-      password: params['user']['password_confirmation']
+      password_confirmation: params['user']['password_confirmation']
     )
     if user
       session[:user_id] = user.id
