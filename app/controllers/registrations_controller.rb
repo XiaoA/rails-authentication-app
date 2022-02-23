8944ef7117ec357bd6d1bd4c8fc503fbd2faf6a9
Andrew Buckingham <abuckingham99@gmail.com>
2022-02-23 15:24:51 -0800

    Add 'username' column to users table

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,9 @@ class RegistrationsController < ApplicationController
     user = User.create!(
       email: params['user']['email'],
       password: params['user']['password'],
-      password_confirmation: params['user']['password_confirmation']
+      password_confirmation: params['user']['password_confirmation'],
+      username: params['user']['username'],
+      team_name: params['user']['team_name']
     )
     if user
       session[:user_id] = user.id
