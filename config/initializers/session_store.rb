d116c0dc073ee3981d9de47ef4075e3295b89b0d
Andrew Buckingham <abuckingham99@gmail.com>
2022-03-18 13:59:09 -0700

    Make sessions httponly

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://aeb-rails-authentication-app.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://aeb-rails-authentication-app.herokuapp.com", httponly: true
 else
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", httponly: true
 end
